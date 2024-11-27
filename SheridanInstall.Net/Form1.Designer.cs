b6d8eb3fe38bc58bf796bbaad30935d5710f5f39
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-11-27 11:00:43 -0500

    Learning how WinForms works for .Net Framework

--- SheridanInstall.Net/Form1.Designer.cs
+++ SheridanInstall.Net/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SlateCheckbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // SlateCheckbox
+            // 
+            this.SlateCheckbox.AutoSize = true;
+            this.SlateCheckbox.Location = new System.Drawing.Point(96, 64);
+            this.SlateCheckbox.Name = "SlateCheckbox";
+            this.SlateCheckbox.Size = new System.Drawing.Size(60, 17);
+            this.SlateCheckbox.TabIndex = 0;
+            this.SlateCheckbox.Text = "SLATE";
+            this.SlateCheckbox.UseVisualStyleBackColor = true;
+            this.SlateCheckbox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SlateCheckbox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox SlateCheckbox;
     }
 }
 
--- SheridanInstall.Net/Form1.cs
+++ SheridanInstall.Net/Form1.cs
@@ -16,5 +16,10 @@ namespace SheridanInstall.Net
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
